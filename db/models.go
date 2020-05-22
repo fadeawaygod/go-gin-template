3cd347134251d3afb8f2c4f58e455f9ea49f08f5
Tom Chen <tom.chen@silkrode.com.tw>
2020-05-22 14:29:42 +0800

    feat: add models

---
+++ db/models.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+	// formysql
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"go-gin-template/config"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	ID         int `gorm:"primary_key" json:"id"`
+	CreatedOn  int `json:"created_on"`
+	ModifiedOn int `json:"modified_on"`
+}
+
+func init() {
+	dbType := config.GetEnv("DB_TYPE")
+	dbName := config.GetEnv("DB_NAME")
+	user := config.GetEnv("DB_USER")
+	password := config.GetEnv("DB_PASSWORD")
+	host := config.GetEnv("DB_HOST")
+
+	db, err := gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		user,
+		password,
+		host,
+		dbName))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return defaultTableName
+	}
+
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+}
+
+// CloseDB close db connection
+func CloseDB() {
+	defer db.Close()
+}
